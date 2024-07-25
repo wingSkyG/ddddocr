4a0722187453f8ed9b3bc806b4f9e8c0086423b0
wingSky <1392004426@qq.com>
2024-07-25 23:18:37 +0800

    新建项目

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    # example.py
+    import ddddocr
+
+    ocr = ddddocr.DdddOcr()
+
+    image = open("D:\\Project\\Engage2024\\DDDDOCR\\image\\3.png", "rb").read()
+    result = ocr.classification(image)
+    print(result)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')

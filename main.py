ea201360a56815310737a284d617b2a797cb9b5b
stao <1392004426@qq.com>
2024-07-26 16:09:48 +0800

    测试目标检测能力

--- main.py
+++ main.py
@@ -15,18 +15,37 @@
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    # example.py
-    import ddddocr
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     # example.py
+#     import ddddocr
+#
+#     ocr = ddddocr.DdddOcr()
+#
+#     image = open("F:\\Code\\Project\\Program\\Engage2024\\ddddocr\\image\\2.png", "rb").read()
+#     result = ocr.classification(image)
+#     print(result)
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
-    ocr = ddddocr.DdddOcr()
+import ddddocr
+import cv2
 
-    image = open("D:\\Project\\Engage2024\\DDDDOCR\\image\\3.png", "rb").read()
-    result = ocr.classification(image)
-    print(result)
+det = ddddocr.DdddOcr(det=True)
 
+with open(r"F:\Code\Project\Program\Engage2024\ddddocr\image\interactive\click_image\1.jpg", 'rb') as f:
+    image = f.read()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+bboxes = det.detection(image)
+print(bboxes)
+
+im = cv2.imread(r"F:\Code\Project\Program\Engage2024\ddddocr\image\interactive\click_image\1.jpg")
+
+for bbox in bboxes:
+    x1, y1, x2, y2 = bbox
+    im = cv2.rectangle(im, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
+
+cv2.imwrite("result.jpg", im)
